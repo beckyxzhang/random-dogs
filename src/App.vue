cf1daa0f2d5409ca5704e1b5c0d86970f58ed58a
Xinyue Becky Zhang <beckyzhangbx1@gmail.com>
2021-01-22 12:30:52 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <HelloWorld  msg="Nine Dogs"></HelloWorld>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <div id="img-grid">
+      <img v-for="(dog, index) in info" :src="dog" width="300" height="300" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      info: null
+    };
+  },
+  mounted() {
+    fetch("https://dog.ceo/api/breeds/image/random/9")
+      .then(response => response.json())
+      .then(response => {
+        this.info = response.message;
+      });
+  }
+};
+</script>
+
+<style scoped>
+#img-grid {
+  display: flex;
+  flex-wrap: wrap;
+  width: 900px;
+}
+</style>
